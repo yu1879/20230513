c87180765746997794bd687dc2e7416c1527f232
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-13 17:09:20 +0800

    c++

--- lesson4_4.cpp
+++ lesson4_4.cpp
@@ -1,9 +1,18 @@
 #include  <iostream>
 
 using namespace std;
-
+string add(string,string);
+int add(int,int);
 int main(int args, char **argv) {
+    cout<<add(1234,5678)<<endl;
+    cout<<add("1234","5678")<<endl;
 
 
     return 0;
+}
+string add(string a,string b){
+    return a+b;
+}
+int add(int a,int b){
+    return a+b;
 }
