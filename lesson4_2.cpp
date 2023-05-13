e7c12af193e628d9f8d0645a88b6d5d88b1f07d3
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-13 15:02:57 +0800

    c++

---
+++ lesson4_2.cpp
@@ -0,0 +1,48 @@
+#include  <iostream>
+#include  <cstdlib>
+#include  <ctime>
+using namespace std;
+
+int main(int args, char **argv) {
+
+    srand(time(0));
+    int lotto[6];
+//大樂透產生器
+//1.產生6個1-49不重覆的整數亂數genLotto
+    for(int m=0; m<6; m++) {
+        lotto[m]=rand()%49+1;
+        //驗證是否有覆
+        for(int p=0; p<m; p++) {
+            if(lotto[m]==lotto[p]) {
+                m--;
+                break;
+            }
+        }
+    }
+
+
+//2.顯示六個號碼showLotto
+    for(int m=0; m<6; m++){
+        cout<<lotto[m]<<"\t";
+        cout<<endl;
+    }
+    for(int m=0;m<5;m++){
+        for(int p=m+1;p<6;p++){
+            if(lotto[m]>lotto[p]){
+                int temp=lotto[m];
+                lotto[m]=lotto[p];
+                lotto[p]=temp;
+            }
+        }
+    }
+
+    cout<<"===========================================\n"<<endl;
+
+    for(int m=0;m<6;m++ )
+
+    {
+        cout<<lotto[m]<<"\t";
+        cout<<endl;
+    }
+    return 0;
+}
