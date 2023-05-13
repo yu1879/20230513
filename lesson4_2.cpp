6f0bb439dd0be96db9f562e10bed5463b9d547eb
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-13 15:40:42 +0800

    c++

--- lesson4_2.cpp
+++ lesson4_2.cpp
@@ -2,14 +2,27 @@
 #include  <cstdlib>
 #include  <ctime>
 using namespace std;
-void genLotto(int[],int);
+void genLotto(int [],int );
+void showLotto(int [],int );
+void sortLotto(int [],int );
+void mySwap(int*,int * );
 int main(int args, char **argv) {
 
     srand(time(0));
     int lotto[6];
+    genLotto(lotto,6);
+    showLotto(lotto,6);
+    sortLotto(lotto,6);
+    showLotto(lotto,6);
 //大樂透產生器
 //1.產生6個1-49不重覆的整數亂數genLotto
-    for(int m=0; m<6; m++) {
+    return 0;
+}
+//void genLotto(int lotto[],int length){
+//
+//}
+void genLotto(int lotto[],int length){
+    for(int m=0; m<length; m++) {
         lotto[m]=rand()%49+1;
         //驗證是否有覆
         for(int p=0; p<m; p++) {
@@ -20,34 +33,91 @@ int main(int args, char **argv) {
         }
     }
 
-
-//2.顯示六個號碼showLotto
-    for(int m=0; m<6; m++){
-        cout<<lotto[m]<<"\t";
-
 }
+void showLotto(int lotto[],int length) {
+    for(int m=0; m<length; m++)
+        cout<<lotto[m]<<"\t";
     cout<<endl;
-    for(int m=0;m<5;m++){
-        for(int p=m+1;p<6;p++){
-            if(lotto[m]>lotto[p]){
-                int temp=lotto[m];
-                lotto[m]=lotto[p];
-                lotto[p]=temp;
+}
+//排序--begin
+void sortLotto(int lotto[],int length){
+    for(int m=0; m<length-1; m++) {
+        for(int p=m+1; p<length; p++) {
+            if(lotto[m]>lotto[p]) {
+                //交換
+                mySwap((lotto+m),&lotto[p]);
             }
         }
     }
 
-    cout<<"===========================================\n"<<endl;
-
-    for(int m=0;m<6;m++ )
-
-    {
-        cout<<lotto[m]<<"\t";
-
-    }
-    cout<<endl;
-    return 0;
 }
-void genLotto(int lotto[],int length){
-
+void mySwap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
 }
+//#include<iostream>
+//#include<cstdlib>
+//#include<ctime>
+//using namespace std;
+//void genLotto(int[],int);
+//void showLotto(int[],int);
+//void sortLotto(int[],int);
+//void mySwap(int *,int *);
+//int main() {
+//    srand(time(0));
+//    int lotto[6];
+////3.將六個號碼由小到大排序sortLotto
+//
+//
+//
+//    cout<<"\n==================================================\n";
+//
+//    return 0;
+//}
+////排序--begin
+//void sortLotto(int lotto[],int length){
+//    for(int m=0; m<length-1; m++) {
+//        for(int p=m+1; p<length; p++) {
+//            if(lotto[m]>lotto[p]) {
+//                //交換
+//                mySwap();
+//            }
+//        }
+//    }
+//
+//}
+////排序--end
+//
+////交換--begin
+//void mySwap(int *a,int *b){
+//    int temp=*a;
+//    *a=*b;
+//    *b=temp;
+//}
+////交換--end
+//
+//
+////印出樂透內容--begin
+//void showLotto(int lotto[],int length) {
+//    for(int m=0; m<length; m++)
+//        cout<<lotto[m]<<"\t";
+//    cout<<endl;
+//}
+////印出樂透內容--end
+//
+////產生大樂透的函數--begin
+//void genLotto(int lotto[],int length) {
+//    for(int m=0; m<length; m++) {
+//        lotto[m]=rand()%49+1;
+//        //驗證是否有覆
+//        for(int p=0; p<m; p++) {
+//            if(lotto[m]==lotto[p]) {
+//                m--;
+//                break;
+//            }
+//        }
+//    }
+//
+//}
+////產生大樂透的函數--end
