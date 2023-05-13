7d1614311f754e973f55f282ff1d97dd96a0790e
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-13 14:25:50 +0800

    c++

--- lesson4_1.cpp
+++ lesson4_1.cpp
@@ -1,19 +1,19 @@
 #include  <iostream>
 
 using namespace std;
-void mySwap(int ,int );
+void mySwap(int * ,int *);
 
 int main(int args, char **argv) {
     system("chcp 65001");
     int a=66,b=33;
     cout<<"呼叫函數的a="<<a<<",b="<<b<<endl;
-    mySwap(a,b);
+    mySwap(&a,&b);
     cout<<"呼叫函數後的a="<<a<<",b="<<b<<endl;
 
     return 0;
 }
-void  mySwap(int a,int b){
-    int temp=a;
-    a=b;
-    b=temp;
+void  mySwap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
 }
